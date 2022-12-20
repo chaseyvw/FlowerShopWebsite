65f1eb254733b116f4586bb4f872413c8cdb749c
chaseyvw <chaseyvw@gmail.com>
2022-12-20 14:20:11 -0800

    jQuery update

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
     
+    var lilacSaved = false;
+    var poppySaved = false;
+    var orchidSaved = false;
+
+    $(".orchidHeart").click(function(){
+        if (orchidSaved == false) {
+            $(".orchidHeart").val("heart");
+            orchidSaved = true;
+        } else {
+            $(".orchidHeart").val("heart_plus");
+            orchidSaved = false;
+        }
+    });
+    
+    
+    
     var galleryContainerWidth = $("#galleryContainer").width();
     var galleryCardContainerWidth = $("#galleryCardsContainer").width();
+    var galleryCardWidth = $(".gallery_card").width();
 
     $("#rightGalleryArrow").click(function(){
+        $("#galleryCardsContainer").animate({left: '-500px'});
+        
+    });
+
+    $("#leftGalleryArrow").click(function(){
         $("#galleryCardsContainer").animate({left: '500px'});
     });
 
